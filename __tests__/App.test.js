a5998e732c7e45131295ec17d0f8524e1f09fe4b
minel <mucahit.inel@hotmail.com>
2018-04-17 21:45:35 +0300

    added test environments

---
+++ __tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../src/App';
+
+it('renders correctly', () => {
+  const snap = shallow(<App />);
+  expect(snap).toMatchSnapshot();
+});
